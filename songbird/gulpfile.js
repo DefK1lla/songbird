9f6211cc516f5c734927194cbd482c962559d468
Sedrak Adoyan <set.adoyan@gmail.com>
2022-11-18 20:38:21 +0400

    feat: add npm script predeploy

--- songbird/gulpfile.js
+++ songbird/gulpfile.js
@@ -16,85 +16,85 @@ const source = require('vinyl-source-stream');
 const buffer = require('vinyl-buffer');
 
 function browser() {
-    browserSync.init({
-        server: {
-            baseDir: 'app/'
-        }
-    });
+  browserSync.init({
+    server: {
+      baseDir: 'app/'
+    }
+  });
 }
 
 function imagesCompressor() {
-    return src('app/images/**/*')
-        .pipe(imageMin([
-            imageMin.gifsicle({ interlaced: true }),
-            imageMin.mozjpeg({ quality: 75, progressive: true }),
-            imageMin.optipng({ optimizationLevel: 5 }),
-            imageMin.svgo({
-                plugins: [
-                    { removeViewBox: true },
-                    { cleanupIDs: false }
-                ]
-            })
-        ]))
-        .pipe(dest('dist/images'))
+  return src('app/assets/**/*')
+    .pipe(imageMin([
+      imageMin.gifsicle({ interlaced: true }),
+      imageMin.mozjpeg({ quality: 75, progressive: true }),
+      imageMin.optipng({ optimizationLevel: 5 }),
+      imageMin.svgo({
+        plugins: [
+          { removeViewBox: true },
+          { cleanupIDs: false }
+        ]
+      })
+    ]))
+    .pipe(dest('dist/assets'))
 }
 
 function scriptsBuilder() {
-    return browserify('app/js/main.js', { debug: true })
-        .transform('babelify', { presets: ['@babel/env'], sourceMaps: true })
-        .bundle()
-        .pipe(source('main.min.js'))
-        .pipe(buffer())
-        .pipe(sourcemaps.init({ loadMaps: true }))
-        .pipe(uglify())
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(dest('app/js'))
-        .pipe(browserSync.stream())
+  return browserify('app/js/main.js', { debug: true })
+    .transform('babelify', { presets: ['@babel/env'], sourceMaps: true })
+    .bundle()
+    .pipe(source('main.min.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
 }
 
 
 function stylesBuilder() {
-    return src('app/scss/style.scss')
-        .pipe(sourcemaps.init())
-        .pipe(scss({ outputStyle: 'compressed' }))
-        .pipe(concat('style.min.css'))
-        .pipe(autoprefixer({
-            overrideBrowserslist: ['last 10 version'],
-            grid: true
-        }))
-        .pipe(sourcemaps.write('./maps'))
-        .pipe(dest('app/css'))
-        .pipe(browserSync.stream())
+  return src('app/scss/style.scss')
+    .pipe(sourcemaps.init())
+    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(concat('style.min.css'))
+    .pipe(autoprefixer({
+      overrideBrowserslist: ['last 10 version'],
+      grid: true
+    }))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(dest('app/css'))
+    .pipe(browserSync.stream())
 }
 
 function cssLibsBuilder() {
-    return src([
-        'node_modules/reset.css/reset.css',
-    ])
-        .pipe(concat('_libs.scss'))
-        .pipe(dest('app/scss'))
+  return src([
+    'node_modules/reset.css/reset.css',
+  ])
+    .pipe(concat('_libs.scss'))
+    .pipe(dest('app/scss'))
 }
 
 function watcher() {
-    watch(['app/scss/**/*.scss'], stylesBuilder);
-    watch(['app/js/**/*.js', '!app/js/main.min.js'], scriptsBuilder);
-    watch(['app/*.html']).on('change', browserSync.reload);
+  watch(['app/scss/**/*.scss'], stylesBuilder);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scriptsBuilder);
+  watch(['app/*.html']).on('change', browserSync.reload);
 }
 
 function builder() {
-    return src([
-        'app/css/style.min.css',
-        'app/fonts/**/*',
-        'app/audio/**/*',
-        'app/js/main.min.js',
-        'app/*.html'
-    ], { base: 'app' })
-        .pipe(dest('dist'))
+  return src([
+    'app/css/style.min.css',
+    'app/fonts/**/*',
+    'app/audio/**/*',
+    'app/js/main.min.js',
+    'app/*.html'
+  ], { base: 'app' })
+    .pipe(dest('dist'))
 }
 
 
 function distCleaner() {
-    return del('dist')
+  return del('dist')
 }
 
 
