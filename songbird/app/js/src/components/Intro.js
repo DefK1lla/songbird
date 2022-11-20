ee40838b93a773e7f5034eccc3e80790007d33c8
Sedrak Adoyan <set.adoyan@gmail.com>
2022-11-20 16:33:40 +0400

    feat: add intro component localization

--- songbird/app/js/src/components/Intro.js
+++ songbird/app/js/src/components/Intro.js
@@ -1,8 +1,17 @@
+import en from '../content/intro/en.json';
+import ru from '../content/intro/ru.json';
+
 import Container from "./Container";
+
+import LocalStorage from '../utils/localStorage';
 
 class Intro extends HTMLDivElement {
   constructor() {
     super();
+
+    const locale = LocalStorage.getLocale();
+
+    this.content = locale === 'en' ? en : ru;
 
     this.className = 'intro';
 
@@ -20,7 +29,7 @@ class Intro extends HTMLDivElement {
 
     this.text = document.createElement('p');
     this.text.className = 'intro__text';
-    this.text.innerHTML = '<b>Songbird</b> is a quiz app for recognizing birds by their voices.';
+    this.text.innerHTML = this.content.text;
     this.inner.append(this.text);
 
     this.links = document.createElement('div');
@@ -30,13 +39,13 @@ class Intro extends HTMLDivElement {
     this.quizLink = document.createElement('a');
     this.quizLink.href = '#/quiz';
     this.quizLink.className = 'intro__btn btn';
-    this.quizLink.textContent = 'Start quiz';
+    this.quizLink.textContent = this.content.btns.quiz;
     this.links.append(this.quizLink);
 
     this.galleryLink = document.createElement('a');
     this.galleryLink.href = '#/gallery';
     this.galleryLink.className = 'intro__btn btn';
-    this.galleryLink.textContent = 'View gallery';
+    this.galleryLink.textContent = this.content.btns.gallery;
     this.links.append(this.galleryLink);
   }
 }
